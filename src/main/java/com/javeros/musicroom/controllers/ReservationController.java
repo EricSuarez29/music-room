0ac6ef39b9d80701ed96000f040a82cae5cbd653
Eric Suarez <ericfernandomou5@gmail.com>
2022-03-24 11:40:41 -0600

    listado de reservaciones

--- src/main/java/com/javeros/musicroom/controllers/ReservationController.java
+++ src/main/java/com/javeros/musicroom/controllers/ReservationController.java
@@ -1,17 +1,29 @@
 package com.javeros.musicroom.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.javeros.musicroom.repository.IReservationRepository;
+
 @Controller
 @RequestMapping("/reservations")
 public class ReservationController {
+	
+	private final IReservationRepository repository;
+	
+	
+	@Autowired
+	public ReservationController(IReservationRepository respository) {
+		this.repository = respository;
+	}
 
 	@GetMapping
 	public String showListReservations(Model model) {
 		model.addAttribute("title", "Reservaciones");
+		model.addAttribute("list", repository.findAll());
 		return "reservation/reservation-list";
 	}
 	
