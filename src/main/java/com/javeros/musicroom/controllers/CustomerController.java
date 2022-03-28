d9d2a040a4dc1384fa8a76dbc73339e7f4191fa9
LEXZTemptation <alejandro_temptation@outlook.com>
2022-03-28 11:30:24 -0600

    Agregando cambios a clientes

---
+++ src/main/java/com/javeros/musicroom/controllers/CustomerController.java
@@ -0,0 +1,63 @@
+package com.javeros.musicroom.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.javeros.musicroom.models.Customer;
+import com.javeros.musicroom.repository.ICustomerRepository;
+import com.javeros.musicroom.repository.IReservationRepository;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController 
+{
+	private final ICustomerRepository customerRepository;
+	
+	@Autowired
+	public CustomerController(ICustomerRepository repository) 
+	{
+		this.customerRepository = repository;
+	}
+	
+	
+	@GetMapping
+	public String showListCustomer(Customer customer, Model model)
+	{
+		model.addAttribute("title", "Clientes");
+		model.addAttribute("list", customerRepository.findAll());
+		
+		return "customers/customers-list";
+	}
+	
+	@PostMapping
+	public String registerCustomer(Customer customer)
+	{
+		customerRepository.save(customer);
+		
+		return "redirect:/customers";
+		
+	}
+	
+	@GetMapping("/form")
+	public String showFormCustomer(Customer customer, Model model)
+	{
+		model.addAttribute("title", "Clientes");
+		return "customers/customers-form";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteById(@PathVariable("id") long id)
+	{
+		customerRepository.setStatusZero(id);
+		return "redirect:/customers";
+	}
+	
+
+}
+
+
